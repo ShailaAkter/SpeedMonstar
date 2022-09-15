0cf226cefe53cf678ed994c2eed8a6d910e50294
Shaila Akter <shailaakterss@gmail.com>
2022-09-15 22:02:48 +0600

    error is showing now

--- SpeedMonstar/script.js
+++ SpeedMonstar/script.js
@@ -46,7 +46,9 @@ const typeController = (e) => {
     display.innerHTML += `<span class="green">${newLetter === " " ? "▪" : newLetter}</span>`;
   } else {
     display.innerHTML += `<span class="red">${newLetter === " " ? "▪" : newLetter}</span>`
- 
+    errorCount++;
+    console.log(errorCount);
+
   }
 
   // check if given question text is equal to user typed text
